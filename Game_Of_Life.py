c2d554dfc563417a587ad7abfb55667c7e92c53a
LJBMyco <ljbradley@hotmail.com>
2023-03-06 23:25:27 +0000

    Tidy up

--- Game_Of_Life.py
+++ Game_Of_Life.py
@@ -34,8 +34,6 @@ class GameOfLife(object):
             self.lattice[start + 1][start - 1] = 1.0
             self.lattice[start][start - 2] = 1.0
 
-    ########## Update the lattice ##########
-
     def update(self) -> None:
         """Do the update following the rules"""
 
@@ -57,87 +55,7 @@ class GameOfLife(object):
         new_lattice[np.where((self.lattice == 1) & (nearest_neighbours < 2))] = 0.0
         self.lattice = np.copy(new_lattice)
 
-    ########## Calculate properties of the living cells ##########
-
     @property
     def total_live(self) -> float:
         """Calculate all living cells"""
         return np.sum(self.lattice)
-
-    """Find centre of mass of a glider"""
-
-    def centre_of_mass(self):
-
-        # Store glider positions
-        X = []
-        Y = []
-        for i in range(self.shape):
-            for j in range(self.shape):
-                if self.lattice[i][j] == 1.0:
-                    # If glider is at the boundary ignore
-                    if (i == 0) or (i == self.shape - 1):
-                        return False
-                    elif (j == 0) or (j == self.shape - 1):
-                        return False
-                    else:
-                        X.append(i)
-                        Y.append(j)
-
-        # Centre of mass is mean of living cells
-        return [np.mean(X), np.mean(Y)]
-
-    ########## Data collection ##########
-
-    """Run model with data collection"""
-
-    def data_collection(self):
-
-        # Run data collection based on initial start conditions
-
-        if self.inital == "glider":
-
-            com_position = []
-            time = []
-            self.create_lattice()
-            for n in range(self.sweeps):
-                self.update()
-                # Ignore if at boundary
-                if not self.centre_of_mass():
-                    continue
-                else:
-                    com_position.append(com)
-                    time.append(n)
-
-            com_position = np.transpose(np.array(com_position))
-            time = np.transpose(np.array(time))
-
-            data = np.transpose(np.vstack(time, com_position))
-            # Save data
-            np.savetxt("com_data.dat", data)
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv) != 5:
-        print("Incorrect Number of Arguments Presented.")
-        print(
-            "Usage: "
-            + sys.argv[0]
-            + "lattice Size, Number of sweeps, inital conditions, Data/Animate"
-        )
-        quit()
-    elif sys.argv[3] not in ["random", "glider"]:
-        print("Please choose either random IC or a glider")
-        quit()
-    elif sys.argv[4] not in ["data", "animate"]:
-        print("Please choose either data or animate")
-        quit()
-    else:
-        shape = int(sys.argv[1])
-        sweeps = int(sys.argv[2])
-        start = sys.argv[3]
-        mode = sys.argv[4]
-
-    model = GameOfLife(shape, sweeps, start)
-    if mode == "data":
-        model.data_collection()
